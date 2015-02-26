0809a0a32631f1fd3eef4f7e1b8f70bc7f1c2fdc
adam <adam@u.nus.edu>
2015-02-26 19:02:45 +0800

    Added test cases for Storage class

--- TextBuddyTest.java
+++ TextBuddyTest.java
@@ -80,5 +80,26 @@ public class TextBuddyTest {
 		assertEquals("Clear", decipherTest.getCommandType());
 		assertFalse(decipherTest.isGoodCommand());
 	}
+	
+	@Test
+	public void testStorage() {
+		
+		Storage storageTest = new Storage("storageTest.txt");
+		storageTest.addLine("do homework by Wed");
+		assertEquals("1. do homework by Wed", storageTest.printLines());
+		
+		storageTest.addLine("complete project today");
+		storageTest.addLine("3ldh5 sjk5h2");
+		assertEquals("1. do homework by Wed\n2. complete project today\n3. 3ldh5 sjk5h2", storageTest.printLines());
+		assertEquals("", storageTest.deleteLine("100"));
+		assertEquals("do homework by Wed", storageTest.deleteLine("1"));
+		assertEquals("3ldh5 sjk5h2", storageTest.deleteLine("2"));
+		assertEquals("1. complete project today", storageTest.printLines());
+		
+		storageTest.clearLines();
+		assertEquals("", storageTest.printLines());
+		assertEquals("", storageTest.deleteLine("1"));
+		assertEquals("", storageTest.deleteLine("10"));
+	}
 
 }
